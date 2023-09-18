9d331f7c508bd7d83bb520e3e4c67f59b86d7190
laissteixeira <134931385+laissteixeira@users.noreply.github.com>
2023-09-18 08:22:44 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import styles from './App.module.css'
+import poweredImage from './assets/powered.png'
+import left from './assets/leftarrow.png'
+import {levels, calculateImc, Level} from './helpers/imc'
+import { GridItem } from './components/GridItem'
+
+const App = () =>{
+
+    const [altura, setAltura] = useState(0);
+    const [peso, setPeso] = useState(0);
+    const [toShow, settoShow] = useState<Level | null>(null);
+
+    const handleCalc = () =>{
+      if (altura && peso){
+        settoShow(calculateImc(altura, peso))
+      }
+      else{
+          alert('Informe a altura e o peso')
+      }
+    }
+
+    const handleBackButton = () => {
+      settoShow(null);
+      setAltura(0);
+      setPeso(0);
+    }
+
+  return(
+    <div className={styles.main}>
+      <header>
+          <div className={styles.headerContainer}>
+            <img src={poweredImage} alt ="" width={150}/>
+          </div>
+      </header>
+        <div className={styles.container}>
+          
+          <div className={styles.leftSide}>
+            <h1>CÁLCULO DO IMC</h1>
+
+            <p>O IMC (Índice de Massa Corporal) é reconhecido pela OMS (Organização Mundial da Saúde)
+               como um padrão internacional que avalia se as pessoas, entre 20 e 59 anos,
+               estão com peso ideal ou em excesso, em relação a sua altura.</p>
+               <br/>
+
+            <input
+             className={styles.input}
+             type="number"
+             placeholder="Informe a altura. Ex. 1.5 (em metro)"
+             value={altura > 0 ? altura: ''}
+             onChange={t => setAltura(parseFloat(t.target.value) )}
+            />
+
+            <input
+             className={styles.input}
+             type="number"
+             placeholder="Informe o peso. Ex. 60.5 (em kilo)"
+             value={peso > 0 ? peso: ''}
+             onChange={t => setPeso(parseFloat(t.target.value) )}
+            />
+
+            <button onClick={handleCalc}>Calcular</button>
+          </div>
+
+          <div className={styles.rightSide}>
+              {!toShow &&
+              <div className={styles.grid}>
+                  {levels.map((item, key) => (
+                    <GridItem key={key} item={item}/>
+                  ))}
+                  </div>
+                  }
+                  {toShow &&
+                  <div className={styles.rightBig}>
+                    <GridItem item={toShow}/>
+                    <div className={styles.rightArrow} onClick={handleBackButton}>
+                      <img src={left} width={25}/>
+                    </div>
+                  </div>
+                  }
+            </div>
+                 
+          </div>
+        </div>
+  )
+}
+
+export default App;
--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -2,27 +2,29 @@ export type Level = {
     title: string;
     color: string;
     icon: 'down' | 'up';
-    imc: number []
+    imc: number[];
     yourImc?: number;
 }
 
-export const levels: Level [] = [
-    {title: 'Magreza', color:'#ff0048', icon: 'down', imc: [0, 18.5]},
-    {title: 'Normal', color:'#4aff59', icon: 'up', imc: [18.6, 24.9]},
-    {title: 'Sobrepeso', color:'#f7a943', icon: 'down', imc: [25, 30]},
-    {title: 'Obesidade', color:'#ff0048', icon: 'down', imc: [30.1, 99]},
+export const levels: Level[] =  [
+  {title: 'Magreza',    color:'#96A3AB', icon:'down', imc:[0, 18.5]},
+  {title: 'Normal',     color:'#0EAD69', icon:'up',   imc:[18.6, 24.9]},
+  {title: 'Sobrepeso',  color:'#E2B039', icon:'down', imc:[25, 30]},
+  {title: 'Obesidade ', color:'#C3423F', icon:'down', imc:[30.1, 99]}
 ];
-
 
 export const calculateImc = (altura:number, peso:number) => {
     const imc = peso / (altura * altura);
 
     for ( let i in levels) {
-        if (imc >= levels[i].imc[0] && imc<= levels[i].imc[1]) {
-            levels[i].yourImc = imc;
-            return levels[i]
+      if ( imc >= levels[i].imc[0] && imc <= levels[i].imc[1])
+      {
+        let levelCopy = {...levels[i]};
 
-        }
+         levelCopy.yourImc = imc;
+         return levelCopy
+      }
     }
-    return null;
+    return null;  
 }
+
